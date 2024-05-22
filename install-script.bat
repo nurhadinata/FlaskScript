eed1c2d66b03c4139744d79a7919847d40e96236
Cahya Nurhadinata <59736388+nurhadinata@users.noreply.github.com>
2024-05-21 23:58:19 -0700

    Update install-script.bat

--- install-script.bat
+++ install-script.bat
@@ -7,5 +7,4 @@ winget install --id Google.Chrome -e
 
 set "GIT_BASH_PATH=C:\Program Files\Git\bin\bash.exe"
 set "SCRIPT_PATH=%cd%\install-script.sh"
-start "" "%GIT_BASH_PATH%"
-REM "%SCRIPT_PATH%"
+start "" "%GIT_BASH_PATH%" "%SCRIPT_PATH%"
