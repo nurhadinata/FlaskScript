b692f217a4cca787103794e8c96f0a37fa1dc718
Cahya Nurhadinata <59736388+nurhadinata@users.noreply.github.com>
2024-05-24 14:18:23 +0800

    Update install-script.bat

--- install-script.bat
+++ install-script.bat
@@ -7,6 +7,21 @@ REM winget install --id Git.Git -e
 
 REM winget install --id Google.Chrome -e
 
+
+@echo off
+echo Installing Git...
+set "installer_url=https://github.com/git-for-windows/git/releases/download/v2.35.1.windows.2/Git-2.35.1.2-64-bit.exe"
+set "installer_name=GitInstaller.exe"
+set "install_dir=%TEMP%\Git"
+
+mkdir "%install_dir%" 2>nul
+cd /d "%install_dir%"
+
+curl -L -o "%installer_name%" "%installer_url%"
+start /wait "" "%installer_name%" /SILENT /NORESTART
+
+echo Git has been installed successfully.
+
 set "GIT_BASH_PATH=C:\Program Files\Git\bin\bash.exe"
 set "SCRIPT_PATH=%cd%\install-script.sh"
 set "SCRIPT_PATH_1=%cd%\install-1.sh"
