9590937896adc9101897023858d50f365fdc8012
Cahya Nurhadinata <59736388+nurhadinata@users.noreply.github.com>
2024-05-27 00:06:43 -0700

    Update install-script-1.bat

--- install_script/install-script-1.bat
+++ install_script/install-script-1.bat
@@ -5,16 +5,21 @@ winget upgrade --all
 echo Installing Git Bash...
 winget install --id Git.Git -e
 
-if %ERRORLEVEL% equ 0 (
-  echo Git installation succesful.
-  echo Continuing Purchase App installation...
-) else (
-  echo Installation failed with error code %ERRORLEVEL%.
-  pause
-  exit /b %ERRORLEVEL%
-)
+if not %ERRORLEVEL% equ 0 GOTO GITFAILED
 
+echo Git installation succesful.
+echo Continuing Purchase App installation...
 set "GIT_BASH_PATH=C:\Program Files\Git\bin\bash.exe"
 set "SCRIPT_PATH_1=%cd%\install_script\install-sh-1.sh"
 set "SCRIPT_PATH_2=%cd%\install_script\install-sh-2.sh"
 start "" /wait "%GIT_BASH_PATH%" "%SCRIPT_PATH_1%"
+GOTO CONTINUE
+
+:GITFAILED
+echo Failed to install Git.
+pause
+exit
+
+:CONTINUE
+
+
