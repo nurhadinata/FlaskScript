33780280a775ed6d80d2eaa2f6a4a32ddd35985b
Cahya Nurhadinata <59736388+nurhadinata@users.noreply.github.com>
2024-05-26 23:59:06 -0700

    Update windows-install-1.bat

--- windows-install-1.bat
+++ windows-install-1.bat
@@ -1,4 +1,20 @@
+@echo off
+
 cd /d %~dp0
+
+echo Checking internet connection...
+ping -n 1 www.google.nl
+cls
+
+if not %ERRORLEVEL% equ 0 GOTO NOCONNECTION
+
 call install_script\install-script-1.bat
+GOTO CONTINUE
 
+:NOCONNECTION
+echo No connection available...
+pause
+exit
 
+:CONTINUE
+exit
