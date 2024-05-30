834488abcd2a39e4e689b5d1222708e25e92729c
Cahya Nurhadinata <59736388+nurhadinata@users.noreply.github.com>
2024-05-30 09:56:31 +0800

    Update install-script-1.bat

--- install_script/install-script-1.bat
+++ install_script/install-script-1.bat
@@ -13,6 +13,10 @@ set "GIT_BASH_PATH=C:\Program Files\Git\bin\bash.exe"
 set "SCRIPT_PATH_1=%cd%\install_script\install-sh-1.sh"
 set "SCRIPT_PATH_2=%cd%\install_script\install-sh-2.sh"
 start "" /wait "%GIT_BASH_PATH%" "%SCRIPT_PATH_1%"
+
+echo Installation 1 completed
+pause
+
 GOTO CONTINUE
 
 :GITFAILED
